5a335f8cc33934f3ac120c42e22bdc05444eb2d5
unknown <cindyauliani0629@gmail.com>
2023-08-03 07:59:39 -0300

    router

---
+++ src/assets/Kategori.js
@@ -0,0 +1,34 @@
+export const kategori = {
+    "kategori" : [
+        {
+            "id": 1,
+           "nama": "make-up",
+           "harga": "Rp 1.000.000,00",
+           "img" :  "1.png",
+        },
+        {
+            "id": 2,
+           "nama": "scincare",
+           "harga": "Rp 5.000.000,00",
+           "img" :  "1.png",
+        },
+        {
+            "id": 3,
+           "nama": "fashion",
+           "harga": "Rp 3.000.000,00",
+           "img" :  "1.png",
+        },
+        {
+            "id": 4,
+           "nama": "acsesoris",
+           "harga": "Rp 3.000.000,00",
+           "img" :  "1.png",
+        },
+        {
+            "id": 5,
+           "nama": "jilbab",
+           "harga": "Rp 3.000.000,00",
+           "img" :  "1.jpg",
+        },
+    ]
+}
---
+++ src/assets/Produk.js
@@ -0,0 +1,127 @@
+export const produk = {
+    "produk" : [
+        {
+            "id": 1,
+           "nama": "Fundition",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 1,
+            "img": "1.png",
+        },
+
+        {
+            "id": 2,
+           "nama": "Conceler",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 1,
+            "img": "1.png",
+        },
+        
+        {
+            "id": 3,
+           "nama": "Cusion",
+           "harga": "Rp 350.000,00",
+           "id_Kategori": 1,
+           "img": "1.png",
+        },
+
+        {
+            "id": 4,
+           "nama": "Masker",
+           "harga": "Rp 350.000,00",
+           "id_Kategori": 2,
+           "img": "1.png",
+        },
+
+        {
+            "id": 5,
+           "nama": "Serum",
+           "harga": "Rp 350.000,00",
+           "id_Kategori": 2,
+           "img": "1.png",
+        },
+
+        {
+            "id": 6,
+           "nama": "Sunsciren",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 2,
+            "img": "1.png",
+        },
+
+        {
+            "id": 7,
+           "nama": "Dress",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 3,
+            "img": "1.png",
+        },
+
+        {
+            "id": 8,
+           "nama": "Cardigan",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 3,
+            "img": "1.png",
+        },
+
+        {
+            "id": 9,
+           "nama": "Crop",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 3,
+            "img": "1.png",
+        },
+
+        {
+            "id": 10,
+           "nama": "Necklace",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 4,
+            "img": "1.png",
+        },
+
+        {
+            "id": 11,
+           "nama": "Belt",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 4,
+            "img": "1.png",
+        },
+
+        {
+            "id": 12,
+           "nama": "Bracelet",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 4,
+            "img": "1.png",
+        },
+
+        {
+            "id": 13,
+           "nama": "Segitiga",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 5,
+            "img": "1.png",
+        },
+
+        {
+            "id": 14,
+           "nama": "Pasmina",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 5,
+            "img": "1.png",
+        },
+
+        {
+            "id": 15,
+           "nama": "Rabani",
+           "harga": "Rp 1.000.000,00",
+            "id_Kategori" : 5,
+            "img": "1.png",
+        },
+
+
+
+
+    ]
+}
